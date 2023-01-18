3553602abb996e17161184bcd79b7d63792aa765
Jean-Clément Sabourin <jeanclement.sabourin@manomano.com>
2023-01-18 21:00:23 +0100

    Init

---
+++ src/main/java/com/ynov/chatbotback/ChatbotBackApplication.java
@@ -0,0 +1,13 @@
+package com.ynov.chatbotback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ynov/chatbotback/ChatbotBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynov.chatbotback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
